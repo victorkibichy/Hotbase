cf434824e9788b4cd8a38db25054da77f3273f90
Kibichy <victorkibichy@gmail.com>
2025-04-17 12:04:22 +0300

    creation of profiles with firebase

---
+++ Hotbase/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Hotbase
+//
+//  Created by Kibichy on 17/04/2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ Hotbase/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Hotbase
+//
+//  Created by Kibichy on 17/04/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
